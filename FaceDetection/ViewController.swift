7be4b57751c68992c2c168436902c0a05392694f
Tim Ng <timothyng2008@me.com>
2019-01-13 12:30:37 +0800

    feat: Add face detection feature

--- FaceDetection/ViewController.swift
+++ FaceDetection/ViewController.swift
@@ -7,13 +7,70 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let image = UIImage(named: "sample2") else { return }
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFit
+        
+        let scaledHeight = view.frame.width / image.size.width * image.size.height
+        
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: scaledHeight)
+        imageView.backgroundColor = .blue
+        
+        view.addSubview(imageView)
+        
+        let request = VNDetectFaceRectanglesRequest { (request, error) in
+            if let error = error {
+                print("Failed to detect faces: \(error)")
+                return
+            }
+            
+            request.results?.forEach({ (result) in
+                print(result)
+                
+                DispatchQueue.main.async {
+                    guard let faceObservation = result as? VNFaceObservation else { return }
+                    
+                    let x = self.view.frame.width * faceObservation.boundingBox.origin.x
+                    
+                    let width = self.view.frame.width * faceObservation.boundingBox.width
+                    let height = scaledHeight * faceObservation.boundingBox.height
+                    
+                    let y = scaledHeight * ( 1 - faceObservation.boundingBox.origin.y) - height
+                    
+                    
+                    let redView = UIView()
+                    redView.backgroundColor = .red
+                    redView.alpha = 0.4
+                    redView.frame = CGRect(x: x, y: y, width: width, height: height)
+                    self.view.addSubview(redView)
+                    
+                    print(faceObservation.boundingBox)
+                }
+            })
+        }
+        
+            guard let cgImage = image.cgImage else { return }
+        
+        DispatchQueue.global(qos: .background).async {
+            let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+            do {
+                try handler.perform([request])
+            } catch let requestError {
+                print("Failed to perform request: \(requestError)")
+            }
+        }
     }
+    
+   
 
 
 }
