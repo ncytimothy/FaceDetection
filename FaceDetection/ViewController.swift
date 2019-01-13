af11a741fb17e51d4deec9989279bbc58c41d136
Tim Ng <timothyng2008@me.com>
2019-01-13 10:15:37 +0800

    Fix: make sure git is up to date

---
+++ FaceDetection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceDetection
+//
+//  Created by Tim Ng on 1/13/19.
+//  Copyright © 2019 timothyng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
